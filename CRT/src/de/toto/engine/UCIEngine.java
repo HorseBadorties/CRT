047eb8076e9b7a7cf4faa15f5da6fbb6363a216a
HorseBadorties <HorseBadorties@whereever.com>
2017-01-05 21:26:47 +0100

    save

--- CRT/src/de/toto/engine/UCIEngine.java
+++ CRT/src/de/toto/engine/UCIEngine.java
@@ -3,7 +3,6 @@ package de.toto.engine;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class UCIEngine {
